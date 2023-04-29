24e49c3ccf730acb914f66e80de6cf4313ea6143
Harjasleen Malvai <harjasleen_malvai@brown.edu>
2023-04-29 14:58:13 -0500

    Testing line by line reading

--- models/src/io.rs
+++ models/src/io.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 
 pub trait LineProcessor {
@@ -22,4 +22,34 @@ where
         Ok(file) => file,
     };
     Ok(io::BufReader::new(file).lines())
+}
+
+pub fn open_file<P>(filename: P) -> io::Result<File>
+where
+    P: AsRef<Path> + Clone,
+{
+    let file = match File::open(filename.clone()) {
+        Err(why) => {
+            println!(
+                "Looking for {:?} in {:?}",
+                filename.as_ref().to_str(),
+                std::env::current_dir().unwrap().display()
+            );
+            panic!("Cannot open file: {}", why)
+        }
+        Ok(file) => return Ok(file),
+    };
+    // Ok(io::BufReader::new(file).lines())
+}
+
+
+fn read_file_line_by_line(filepath: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open(filepath)?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        println!("{}", line?);
+    }
+
+    Ok(())
 }
